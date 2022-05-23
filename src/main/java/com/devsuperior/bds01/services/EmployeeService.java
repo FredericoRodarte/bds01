ada3aca2fead6a69f424e5b6b0009d503e389e21
Frederico Rodarte <fredericorodarte12@gmail.com>
2022-05-23 11:17:50 -0300

    Test passing

---
+++ src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.devsuperior.bds01.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds01.dto.EmployeeDTO;
+import com.devsuperior.bds01.entities.Employee;
+import com.devsuperior.bds01.repositories.DepartmentRepository;
+import com.devsuperior.bds01.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository repository;
+	
+	@Autowired
+	private DepartmentRepository Deprepository;
+	
+	@Transactional (readOnly = true)
+	public Page<EmployeeDTO> findAllPaged(Pageable pageable){
+	
+		Page<Employee> page =  repository.findAll(pageable);
+		
+		return page.map(x -> new EmployeeDTO(x));
+		
+	}
+	
+	@Transactional
+	public EmployeeDTO insert(EmployeeDTO dto) {
+		
+		Employee entity = new Employee();
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setDepartment(Deprepository.getOne(dto.getDepartmentId()));
+		entity = repository.save(entity);
+		return new EmployeeDTO(entity);
+		
+	}
+}
